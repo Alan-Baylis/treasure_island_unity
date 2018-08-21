289d87008f198e196131c11348d08090b25fa005
Vyaas <saayvsaayv@gmail.com>
2018-08-20 22:35:44 -0500

    first and only commit

---
+++ Assets/Script.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+
+    public float speed;
+    public float rotationSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        speed = 10.0f;
+        rotationSpeed = 100.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vTranslation = Input.GetAxis("Vertical") * speed;
+        float hTranslation = Input.GetAxis("Horizontal") * speed;
+        float hrotation = Input.GetAxis("Mouse X") * rotationSpeed;
+		float vrotation = Input.GetAxis("Mouse Y") * rotationSpeed;
+
+
+        vTranslation *= Time.deltaTime;
+        hTranslation *= Time.deltaTime;
+        hrotation *= Time.deltaTime;
+		vrotation *= Time.deltaTime;
+
+        transform.Translate(hTranslation, 0, vTranslation);
+        transform.Rotate(vrotation, hrotation, 0);
+    }
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "yellowBrick") {
+			Destroy (other.gameObject);
+		}
+	}
+}
